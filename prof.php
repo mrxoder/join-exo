d8a4545d362543e9e1aa97e20d0ec1b8a55017c7
lonewolf <xoderxoderx@gmail.com>
2022-12-03 10:22:32 -0500

    modif1

--- prof.php
+++ prof.php
@@ -7,16 +7,23 @@
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<meta name="generator" content="Geany 1.38" />
 	<link rel="stylesheet" href="boot/css/bootstrap.min.css"/>
-	
-	<style>
-	   .form{
-		  margin:3% 0 0 3%;
-	   }
-	</style>
+	<link rel="stylesheet" href="style.css" />
 </head>
 
-<body>
+<body class="bg-light">
+	 
+  <div class="sidebar">
+	  <a href="index.php"  >School</a>
+	  <a href="classe.php"  >Classe</a>
+	  <a href="student.php">Etudiant</a>
+	  <a href="prof.php" class="active">Professeur</a>
+	  <a href="logout.php">Quitter</a>
+  </div>
 
+
+<div class="content">
+  
+</div> 
 
 <?php
 
@@ -28,54 +35,8 @@ include("db.php");
 if(empty($_SESSION["username"])){ header("location: login.php");}
 
 $db = new sql();
-$userinfo = $db->getUser($_SESSION["username"]);
-if($userinfo["role"]=="student"){ header("location: index.php");}
-$name = $userinfo["name"];
-
-
-if(!empty($_POST["class"]) && !empty($_POST["course"]) && !empty($_POST["name"])){
-	$newname = "";
-	if($name!=$_POST["name"]){ $newname=$_POST["name"];}
-	if($db->newprof($_POST["class"], $_POST["course"], $_SESSION["username"], $newname) ){
-	   header("location: index.php");
-	}else{
-	   echo("<span>Failed to save Prof profile.</span>");
-	}
-}
-
-$classes = $db->getclass();
-$courses = $db->getcourses();
 
 ?>
-	<div class="container" align="center">
-	
-	<form action="" method="POST" class="form">
-		<div class="row">Name:<input name="name" type="text" value="<?php echo(htmlentities($name));?>" /></div>
-	
-		<div class="row">
-		  <label>Class : </label>
-		  <select name="class">
-			<?php
-			  foreach($classes as $item){
-				  echo("<option value='{$item["class_name"]}'>{$item["class_name"]}</option>");
-			  }
-			?>			
-		  </select>
-		</div>
-		
-		<div class="row">
-		  <label>Teach: </label>
-		  <select name="course">
-			<?php
-			  foreach($courses as $item){
-				  echo("<option value='{$item["course_name"]}'>{$item["course_name"]}</option>");
-			  }
-			?>
-		  </select></div>
-		
-		<div class="row">  
-		<input type="submit" value="Save"/></div>
-	</form>
-	</div>
+
 </body>
 </html>
--- student.php
+++ student.php
@@ -7,9 +7,23 @@
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<meta name="generator" content="Geany 1.38" />
 	<link rel="stylesheet" href="boot/css/bootstrap.min.css"/>
+<link rel="stylesheet" href="style.css" />
 </head>
 
-<body>
+<body class="bg-light">
+	 
+  <div class="sidebar">
+	  <a href="index.php"  >School</a>
+	  <a href="classe.php" >Classe</a>
+	  <a href="student.php" class="active">Etudiant</a>
+	  <a href="prof.php">Professeur</a>
+	  <a href="logout.php">Quitter</a>
+  </div>
+
+
+<div class="content">
+  
+</div> 
 
 
 <?php
@@ -18,6 +32,7 @@ session_start();
 if(empty($_SESSION["username"])){ header("location: login.php");}
 include("db.php");
 $db = new sql();
+?>
 
 </body>
 </html>
